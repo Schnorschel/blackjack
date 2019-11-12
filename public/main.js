2d9ab3950a08fc1f4727081207ce5a1ee2d75daa
Georg Oehl <oehl@pobox.com>
2019-11-11 21:07:32 -0500

    Cosmetics: add/revise comments

--- public/main.js
+++ public/main.js
@@ -1,13 +1,16 @@
+// Blackjack by Georg Oehl (oeh@pobox.com)
+// for end-of-week-two (JavaScript) assignment
+// at Suncoast Developers Guild (SDG)
+
 let deck = [] // The main card deck
-let spentDeck = [] // Cards taken off the main deck are stored here (not used)
 let player1Hand = [] // Player 1 hand
 let dealerHand = [] // Dealer hand
-const wins = [0, 0]
+const wins = [0, 0] // The win scores for dealer and player(s)
 const show = true // A verbose constant for indicating whether to show or hide a card when being dealt
 const hide = false
-const showHighestOnly = true
+const showHighestOnly = true // Another verbose constant to indicate whether to show the highest total value of a hand or the array of all values
 const values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] // Array for assigning values to each card
-const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'] // Suits helper array
 const ranks = [
   'Ace',
   '2',
@@ -22,17 +25,18 @@ const ranks = [
   'Jack',
   'Queen',
   'King'
-]
+] // Ranks helper array
 
 let i, j
 
-// Some functions to allow short-hand of often-used querySelector method calls
+// Some functions to allow a short-hand of often-used querySelector method calls
 const qS = e => document.querySelector(e)
 const qSA = e => document.querySelectorAll(e)
-
 const qStC = (cl, cont) => (document.querySelector(cl).textContent = cont)
 
-// Create and populate the data structure (array) that holds the default card deck of 52 cards
+// Populate the data structure (array) that holds the default card deck of 52 cards
+// I did not store image file names in an attribute, instead deriving it on-the-fly
+// from rank and suit attribute (see function cardImageName)
 const makeDeck = () => {
   for (let i = 0; i < suits.length; i++) {
     for (let j = 0; j < ranks.length; j++) {
@@ -43,26 +47,13 @@ const makeDeck = () => {
 
 // Remove all child tags of a tag given in className.
 // This is used in the reset-game routine to remove
-// all images.
+// all card images from the previous game.
 const removeAllChildren = className => {
-  // const container = document.getElementById(className)
-  // const elements = document.getElementsByClassName(className)
-
-  // while (elements[0]) {
-  //   elements[0].parent.removeChild(elements[0])
-  // }
   const e = document.querySelector(className)
   let first = e.firstElementChild
   while (first) {
     first.remove()
     first = e.firstElementChild
-  }
-}
-
-// Display the card deck given in array 'hand' in the console
-const showCards = hand => {
-  for (let i = 0; i < hand.length; i++) {
-    console.log(hand[i].rank + ' of ' + hand[i].suit)
   }
 }
 
@@ -77,19 +68,15 @@ const shuffleDeck = hand => {
   }
 }
 
-// Calculate and return the total value of cards in given deck/hand (deprecated)
-const getDeckValue = deck => {
-  let sum = 0
-  for (let i = 0; i < deck.length; i++) {
-    sum += deck[i].value
-  }
-  return sum
-}
-
 // Calculate the total value of cards given in 'deck' with aces
-// counting as 1 or 11
-// Returns an array of all possible scores 21 or lower,
-// except if the only score is greater than 21
+// counting as 1 or 11.
+// Returns an array of all possible hand total values 21 or lower,
+// except if the only total value is greater than 21, then
+// that total value will be saved in the array.
+// showHighestOnly is a boolean argument that, when set to true, returns
+// the highest total value only. This is used when the game as over, since
+// only one total value should be displayed, not all possible ones.
+// showHighestOnly set to false or omitted will return the array mentioned above.
 const getDeckValueArray = (deck, showHighestOnly) => {
   const valueArray = []
   let noOfAces = 0
@@ -99,7 +86,7 @@ const getDeckValueArray = (deck, showHighestOnly) => {
   }
 
   // Calculate the total value of the hand with all aces counting as 1 only.
-  // This will be the lowest possible total (if aces are present).
+  // This will be the lowest possible total total value of the hand (if aces are present).
   for (let i = 0; i < deck.length; i++) {
     if (deck[i].value === 11) {
       noOfAces++
@@ -110,6 +97,7 @@ const getDeckValueArray = (deck, showHighestOnly) => {
   }
   // Add the first (=lowest) hand total value to the array
   valueArray.push(sum)
+
   // If that total is greater than 21 it's a bust and additional totals
   // are not needed, so return with the value array to the caller
   if (sum > 21) {
@@ -141,13 +129,9 @@ const getDeckValueArray = (deck, showHighestOnly) => {
   }
 }
 
-// Show the total score of cards given in hand in
-// HTML tag className and return the score to the caller
+// Show the total value of cards given in hand in
+// HTML tag className and return the total value to the caller
 const showScore = (hand, className, showHighestOnly) => {
-  // const score = getDeckValue(hand)
-  // qS(className).textContent = score
-  // return score
-
   // Compose the comma-delimited list of possible total hand values, but
   // replace the last comma with an 'or'
   const valueArray = getDeckValueArray(hand)
@@ -161,8 +145,8 @@ const showScore = (hand, className, showHighestOnly) => {
         : '') + valueArray[i]
   }
 
-  // Display the hand total with the tag for which the class name has been provided.
-  // If class name is missing, skip
+  // Display the hand total in the tag for which the class name has been provided.
+  // If class name is missing, skip.
   if (typeof className !== 'undefined') {
     if (typeof showHighestOnly !== 'undefined') {
       qS(className).textContent = showHighestOnly
@@ -186,28 +170,24 @@ const cardName = card => {
 // needs to have the progress bar at exactly 50%.
 // A score of 1:2 has it at 33% = 1/(1+2) = 1/3
 const displayNoOfWins = (countHouse, countPlayer) => {
-  // qStC(className, ' (' + count + ' wins)')
   // Update the text score
   qStC('.score', countHouse + ':' + countPlayer)
-  // console.log('wins[0]: ' + wins[0] + ', wins[1]: ' + wins[1])
-  // console.log('In: ' + Math.floor(100 * (wins[0] / (wins[0] + wins[1]))) + '%')
   // If dealer score (wins[0]) and player 1 score (wins[1]) are both zero, the formula to
-  // calculate the percentage of the progress bar will fail, so that case has to be caught
-  // and dealt with specially
+  // calculate the percentage of the progress bar will fail, because of a division by zero,
+  // so that case has to be caught and dealt with separately
   if (wins[0] + wins[1] === 0) {
     qS('#progress-bar-percentage').style.width = '50%'
   } else {
     qS('#progress-bar-percentage').style.width =
       Math.floor(100 * (wins[0] / (wins[0] + wins[1]))) + '%'
   }
-  // console.log('Width: ' + qS('#progress-bar-percentage').style.width)
 }
 
 // Return the path of the corresponding image given the card object in card
 const cardImageName = card => {
   // Returns the image file name of a card given its object reference
   // - Assemble the card's name from its properties rank and suit
-  // - Convert it to lowercase
+  // - Convert it to all lowercase
   // - Replace all spaces with underscores
   // - Add a '2' at the end, if the rank is a name, because only the
   //   face cards ending in '2' show pictures
@@ -271,21 +251,19 @@ const checkScore = () => {
   displayNoOfWins(wins[0], wins[1])
 }
 
+// Check if hand is a blackjack by checking the hand value against 21 and
+// counting the cards in the hand
 const isBlackjack = hand => {
   return getDeckValueArray(hand, showHighestOnly) === 21 && hand.length === 2
 }
 
 // Show top of card in deck in tag className
 const layDownTopCard = (deck, className, showHide) => {
-  // Depending on boolean argument showHide, show the card back image or not
+  // Depending on boolean argument showHide, show the card back image
   // or the actual card image.
   // showHide == true: show image, i.e. image file name is assigned to src attribute
   // showHide == false: show back card image, i.e. image file name is assigned to alt attribute
-  // Code for name of cards
-  // qS(className).textContent = cardName(
-  //   deck[deck.length - 1]
-  // )
-  // console.log(cardName(deck[deck.length - 1]))
+
   const cardImg = document.createElement('img')
   if (showHide) {
     cardImg.src = '/images/' + cardImageName(deck[deck.length - 1])
@@ -295,7 +273,6 @@ const layDownTopCard = (deck, className, showHide) => {
     cardImg.alt = '/images/' + cardImageName(deck[deck.length - 1])
     cardImg.src = '/images/card_back.svg'
   }
-  // console.log(className, cardImg.img, cardImg.alt)
   qS(className).appendChild(cardImg)
 }
 
@@ -308,9 +285,6 @@ const disableButtons = className => {
 
 // Deal a card to Player 1
 const dealCardToPlayer1 = () => {
-  // console.log('Event: dealCard.click')
-  // moveCardFromTo(deck, player1Hand)
-  // layDownTopCard(player1Hand, '.cardsOfPlayer1Container', show)
   dealCardToPlayer(deck, player1Hand, '.cardsOfPlayer1Container', show)
   // Player 1 busts
   if (showScore(player1Hand, '.player1Score') === 21) {
@@ -325,20 +299,17 @@ const dealCardToPlayer1 = () => {
     disableButtons()
     qS('.footer').style.display = 'flex'
   }
-  // showNoOfCards(deck, '.noOfCards')
 }
-
+// Deal a new card from fromDeck to toHand and display the card in tag
+// with class className. Boolean argument showHide determines whether
+// card is face up (true) or face down (false). show and hide have been
+// defined as global constants to serve as verbose arguments.
 const dealCardToPlayer = (fromDeck, toHand, className, showHide) => {
   moveCardFromTo(fromDeck, toHand)
   layDownTopCard(toHand, className, showHide)
 }
 
-// Show the number of cards in deck given HTML tag's className
-const showNoOfCards = (deck, className) => {
-  qS(className).textContent = deck.length
-}
-
-// Run the initial steps, like giving two cards to dealer and (each) player,
+// Run the initial steps, giving two cards to dealer and (each) player,
 // show total card value for player(s) only
 const beginGame = () => {
   // Deal to player, dealer, player, dealer
@@ -352,7 +323,7 @@ const beginGame = () => {
   }
 }
 
-// Show, hide or flip all cards over given the container class in className.
+// Show, hide or flip/toggle all cards given the container class in className.
 // This is done by swapping the contents of the src and alt attributes.
 // The swappable content is the file name of the back card image
 // (card_back.svg) and the file name of the card image.
@@ -366,6 +337,7 @@ const flipCards = (className, type, count) => {
   if (typeof type === 'undefined') {
     type = 'show'
   }
+  // Iterate over all images contained inside HTML-tag with class className
   for (let i = 0; i < Math.min(qS(className).children.length, count); i++) {
     let flip = false
     const alt = qS(className).children[i].alt
@@ -389,7 +361,6 @@ const flipCards = (className, type, count) => {
         break
     }
     if (flip) {
-      // console.log(temp + ' -> ' + qS(className).children[i].src)
       qS(className).children[i].alt = qS(className).children[i].src
       qS(className).children[i].src = alt
     }
@@ -397,7 +368,7 @@ const flipCards = (className, type, count) => {
 }
 
 // The steps to run after the player holds/stands and the dealer/house
-// finishes the game
+// plays until finishing the game
 const housePlays = () => {
   // Show the dealer's cards' faces
   flipCards('.cardsOfHouseContainer', 'show')
@@ -405,31 +376,34 @@ const housePlays = () => {
   while (showScore(dealerHand, '.houseScore') < 17) {
     dealCardToPlayer(deck, dealerHand, '.cardsOfHouseContainer', show)
   }
+  // Dealer is done playing, so show the highest values of hands for all players instead of
+  // all possible scores that can occur when aces are present
   showScore(dealerHand, '.houseScore', showHighestOnly)
   showScore(player1Hand, '.player1Score', showHighestOnly)
   // Check all player's card value totals and display who is winner/loser
   checkScore()
   // Disable the Player's buttons
   disableButtons()
-  // show reset button
+  // Show the (enabled) reset button
   qS('.footer').style.display = 'flex'
 }
 
+// Set up new game after player has pressed 'Play again' button
 const resetGame = () => {
-  // - Refresh deck array and shuffle again
+  // Refresh deck array and shuffle again
   deck = []
   makeDeck()
   shuffleDeck(deck)
-  // - Clean out dealer and player hands
+  // Clean out dealer and player hands
   dealerHand = []
   player1Hand = []
-  // - Clean out winner/lose message
+  // Clean out winner/loser message
   qS('.message').innerHTML = '&nbsp;'
-  // - Clean the card scores
+  // Clean the card scores
   for (i = 0; i < qSA('.playerScore').length; i++) {
     qSA('.playerScore')[i].innerHTML = '&nbsp;'
   }
-  // - Hide reset button
+  // Hide reset button
   qS('.footer').style.display = 'none'
   // Re-enable buttons
   for (let i = 0; i < qSA('.buttonDisablable').length; i++) {
@@ -443,6 +417,7 @@ const resetGame = () => {
   beginGame()
 }
 
+// Prompt user with dialog box to enter player's name and display it
 const enterPlayer1Name = () => {
   let newName = ''
 
@@ -453,24 +428,15 @@ const enterPlayer1Name = () => {
 }
 
 const main = () => {
-  // if (document.querySelector('h1.hello-world')) {
-  //   document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  // }
-  console.log('In main')
+  // Populate the deck array with card objects by utilizing the helper arrays
   makeDeck()
-  // showNoOfCards(deck, '.noOfCards')
-  console.log('Made deck')
-  // console.log('Showing cards:')
-  // showCards(deck)
-  // console.log()
-  console.log('Shuffling cards.')
+  // Shuffle the deck just created
   shuffleDeck(deck)
-  // console.log('Showing cards:')
-  // showCards(deck)
+  // Start playing the game by dealing first cards and checking scores for blackjacks
   beginGame()
-  //
 }
 
+// Event handlers
 document.addEventListener('DOMContentLoaded', main)
 qS('.hitMePlayer1').addEventListener('click', dealCardToPlayer1)
 qS('.standPlayer1').addEventListener('click', housePlays)
